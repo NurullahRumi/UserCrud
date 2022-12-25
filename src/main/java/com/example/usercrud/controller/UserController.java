c58af9a26ac48d1cc481eb4ee35dcea82a862b2e
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2022-12-25 23:05:36 +0600

    Uncomment Controller

--- src/main/java/com/example/usercrud/controller/UserController.java
+++ src/main/java/com/example/usercrud/controller/UserController.java
@@ -24,39 +24,39 @@ public class UserController {
         return "add_user";
     }
 
-//    @PostMapping("/adduser")
-//    public User addUser(@RequestBody User user){
-//        return userService.saveUser(user);
-//    }
-//
-//    @PostMapping("/addUsers")
-//    public List<User> addUsers(@RequestBody List<User> users){
-//        return userService.saveUsers(users);
-//    }
-//
-//    @GetMapping("/getUsers")
-//    public List<User> getUsers(){
-//        return userService.fetchUsers();
-//    }
-//
-//    @GetMapping("/getUserById/{id}")
-//    public User getUserById(@PathVariable Long id){
-//        return userService.fetchUserById(id);
-//    }
-//
-//    @GetMapping("/getUserByName/{name}")
-//    public User getUserByName(@PathVariable String name){
-//        return userService.fetchUserByName(name);
-//    }
-//
-//    @DeleteMapping("/removeUserById/{id}")
-//    public String removeUserById(@PathVariable Long id){
-//        userService.deleteUserById(id);
-//        return "User deleted " + id;
-//    }
-//
-//    @PutMapping("/update")
-//    public User editUser(@RequestBody User user){
-//        return userService.updateUser(user);
-//    }
+    @PostMapping("/adduser")
+    public User addUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @PostMapping("/addUsers")
+    public List<User> addUsers(@RequestBody List<User> users){
+        return userService.saveUsers(users);
+    }
+
+    @GetMapping("/getUsers")
+    public List<User> getUsers(){
+        return userService.fetchUsers();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.fetchUserById(id);
+    }
+
+    @GetMapping("/getUserByName/{name}")
+    public User getUserByName(@PathVariable String name){
+        return userService.fetchUserByName(name);
+    }
+
+    @DeleteMapping("/removeUserById/{id}")
+    public String removeUserById(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return "User deleted " + id;
+    }
+
+    @PutMapping("/update")
+    public User editUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
 }
